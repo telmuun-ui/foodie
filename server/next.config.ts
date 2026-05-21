7ac39fb993a30db9a01edc283347b274dc002d2a
25LP5308 25LP5308 <telmuunotgonbileg@gmail.com>
2026-05-21 11:23:20 +0800

    remove cors from next config

--- server/next.config.ts
+++ server/next.config.ts
@@ -9,27 +9,6 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
   serverExternalPackages: ["mongoose", "nodemailer", "bcryptjs"],
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
